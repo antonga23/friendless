5045c3fe9389f705ba3590ddbf4d7fedeaf96be0
Nicholas Strick <strickolas719@gmail.com>
2018-11-11 18:42:19 -0500

    Trying to fix a merge conflict

--- Facebook.py
+++ Facebook.py
@@ -1,32 +1,26 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
-import requests
+import datetime
 import hashlib
 import random
 import time
+import unicodecsv as csv
 
 
 class Facebook:
     def __init__(self, email, password, max_wait=10, site="https://www.facebook.com/", geckopath='/usr/local/bin/geckodriver'):
         self.driver = webdriver.Firefox(executable_path=geckopath)
         self.driver.get(site)
-        self.login(email, password)
         self.max_wait = max_wait
+        self.login(email, password)
 
     def login(self, email, password):
         self.driver.find_element_by_name('email').send_keys(email)
         self.driver.find_element_by_name('pass').send_keys(password)
-        time.sleep(random.uniform(0, self.max_wait))
+        time.sleep(random.uniform(0, self.max_wait * (1/6)))
         self.driver.find_element_by_id('loginbutton').click()
 
     def goto_page(self, site):
         self.driver.get(site)
-
-    def scrape(self, site):
-        response = requests.get(site)
-        soup = BeautifulSoup(response.content, 'lxml')
-        f = soup.find('div', attrs={'class': '_4-u3 _5sqi _5sqk'})
-        print(f.find('span', attrs={'class': '_52id _50f5 _50f7'}))
 
     def scroll(self, x, y):
         self.driver.execute_script("window.scrollTo(" + str(x) + ", " + str(y) + ")")
@@ -48,9 +42,30 @@ class Facebook:
                 break
             last_height = new_height
 
-    def scrape_mobile(self, number_of_pages):
+    def scrape_mobile(self, number_of_pages, scrape_who_reacted=False, file_out='output.csv', hush_params=()):
+
+        list_of_years = ["2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009"]
+        year_pointer = 0
+
+        # Test the hush params.
+        try:
+            can_hush = True
+            if 4 >= len(hush_params) >= 2:
+                x = Facebook.convert_to_time(hush_params[0])
+                y = Facebook.convert_to_time(hush_params[1])
+                if x != y:
+                    can_hush = True
+                else:
+                    can_hush = False
+                print("Enabling hush to sleep at", hush_params[0], "and wake at", hush_params[1])
+            else:
+                can_hush = False
+        except:
+            can_hush = False
+            print("Invalid hush parameters.")
+
         # Print column headers
-        df = [["poster", "timestamp", "text", "num_likes", "num_loves", "num_hahas", "num_wows", "num_sads", "num_angrys", "num_reacts", "event_link", "hashtags_used", "reacted"]]
+        df = [["text", "num_likes", "num_loves", "num_hahas", "num_wows", "num_sads", "num_angrys", "num_reacts", "event_link", "hashtags_used", "who_reacted"]]
         links_to_stories = []
         # Scrape link of "Full Story" for each post.
         for page in range(0, number_of_pages):
@@ -58,10 +73,27 @@ class Facebook:
             for story in stories:
                 links_to_stories.append(story.get_attribute('href'))
             time.sleep(random.uniform(0, self.max_wait))
-            self.driver.find_element_by_xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div[2]/div[2]/a").click()
-
+            try:
+                x = self.driver.find_element_by_xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div[2]/div[2]/a")
+                x.click()
+            except:
+                x = self.driver.find_element_by_xpath("//*[contains(text(), 'Full Story')]")
+                x.click()
+                try:
+                    self.driver.back()
+                    x = self.driver.find_element_by_xpath("//*[contains(text(), '" + list_of_years[year_pointer] + "')]")
+                    x.click()
+                    list_of_years += 1
+                except:
+                    # exit()
+                    pass
         # Traverses each story link and scrapes data
         for link in links_to_stories:
+
+            # Checks if the program should be put to sleep.
+            if can_hush:
+                Facebook.hush(str(hush_params[0]), str(hush_params[1]), seed=int(hush_params[2]), hit_box=int(hush_params[3]))
+
             time.sleep(random.uniform(0, self.max_wait))
             self.goto_page(link)
             # Scrape data
@@ -70,6 +102,7 @@ class Facebook:
             except:
                 poster = ""
             try:
+                # Currently broken.
                 timestamp = self.parse_fb_date(self.driver.find_element_by_xpath("/html/body/div/div/div[2]/div/div[1]/div[1]/div/div[2]/div[1]/abbr").text)
             except:
                 timestamp = ""
@@ -92,7 +125,7 @@ class Facebook:
             num_wows = 0  # done
             num_sads = 0  # done
             num_angrys = 0  # done
-            reacted = []
+            who_reacted = []
 
             # Click the reactions icon.
             self.driver.find_element_by_xpath('//a[contains(@href, "reaction/profile")]').click()
@@ -120,13 +153,13 @@ class Facebook:
 
             num_reacts = num_likes + num_loves + num_hahas + num_wows + num_sads + num_angrys  # done
 
-            while True:
+            while scrape_who_reacted:
                 try:
                     for x in range(1, 100):
                         try:
                             name = self.driver.find_element_by_xpath("/html/body/div/div/div[2]/div/table/tbody/tr/td/div/ul/li[" + str(x) + "]/table/tbody/tr/td/table/tbody/tr/td[3]/div/h3[1]").text
                             name = self.hash_sha256(name)  # Mandatory
-                            reacted.append(name)
+                            who_reacted.append(name)
                         except:
                             break
                     time.sleep(random.uniform(0, self.max_wait))
@@ -134,10 +167,24 @@ class Facebook:
                 except:
                     break
 
-            df.append([poster, timestamp, text, num_likes, num_loves, num_hahas, num_wows, num_sads, num_angrys, num_reacts, event_link, hashtags_used, reacted])
+            print(" post text: |" + text + "\"\n" +
+                  ".num_likes: | " + str(num_likes) + "\n" +
+                  ".num_loves: | " + str(num_loves) + "\n" +
+                  ".num_hahas: | " + str(num_hahas) + "\n" +
+                  "..num_wows: | " + str(num_wows) + "\n" +
+                  "..num_sads: | " + str(num_sads) + "\n" +
+                  "num_angrys: | " + str(num_angrys) + "\n" +
+                  "num_reacts: | " + str(num_reacts) + "\n" +
+                  "event_link: | " + str(event_link) + "\n" +
+                  "hashtags_used: " + str(hashtags_used) + "\n" +
+                  "who_reacted:" + str(who_reacted) + "\n\n")
+
+            df.append([str(text), str(num_likes), str(num_loves), str(num_hahas), str(num_wows), str(num_sads), str(num_angrys), str(num_reacts), str(event_link), str(hashtags_used), str(who_reacted)])
+            with open(file_out, 'ab') as csv_file:
+                writer = csv.writer(csv_file, lineterminator='\n')
+                writer.writerows(df)
+            csv_file.close()
         return df
-
-
 
     @staticmethod
     def hash_sha256(string):
@@ -168,6 +215,7 @@ class Facebook:
         }[month]
 
     def parse_fb_date(self, date, current_year=2018):
+        # Currently broken.
         month = date[0:date.find(" ")]
         date = date.replace(month, "").strip()
         month = self.__enum_month__(month)
@@ -204,3 +252,43 @@ class Facebook:
                 list_out.append(word)
         return list_out
 
+    def immitate_sleep(self, t_sleep, t_wake):
+        print(datetime.datetime.now())
+
+    @staticmethod
+    def right_now():
+        now = str(datetime.datetime.now())
+        return [datetime.timedelta(hours=int(now[11:13]), minutes=int(now[14:16]), seconds=int(now[17:19])), int(now[0:4]), int(now[5:7]), int(now[8:10])]
+
+    @staticmethod
+    def convert_to_time(t):
+        return datetime.timedelta(hours=int(t[0:2]), minutes=int(t[3:5]))
+
+    @staticmethod
+    def get_seconds(timedelta):
+        t = str(timedelta)[-8:-3]
+        if len(t) == 4:
+            t = "0" + t
+        return (float(t[0:2]) + (float(t[3:5]) / 60)) * 3600
+
+    @staticmethod
+    def hush(t_sleep, t_wake, seed=1800, hit_box=600):
+        """
+        Boolean returns are used in case of feeding
+        the hush() function sleep/wake times from a
+        cyclical/linear list. If True is returned,
+        we know to move on to the next sleep/wake.
+        """
+        if t_sleep == t_wake:
+            return True
+        t_sleep = Facebook.convert_to_time(t_sleep)
+        d = abs(t_sleep - Facebook.right_now()[0])
+        if Facebook.get_seconds(d) <= hit_box:
+            t_wake = Facebook.convert_to_time(t_wake)
+            d = t_wake - t_sleep
+            d = Facebook.get_seconds(d)
+            r = random.uniform(-seed, seed)
+            print("Nighty-night... ", Facebook.right_now()[0])
+            time.sleep(d + r)
+            print("I'm up! ", Facebook.right_now()[0])
+            return True
---
+++ README.md
@@ -0,0 +1,51 @@
+# _Friendless_, a Post-CA Facebook Scraper [2018 Edition]
+### Introduction
+Does the bulky Facebook API access got you ticked? Want to
+ just download a script and instantly scrape entire pages?
+ Look no further! I am proud to present to you a page scraper
+ that laughs in the face of API access!
+ 
+This is a post-Cambridge Analytica, post-GDPR and GDPR-compliant
+ scraper with built in PII-hashing so that it may
+ be used in something like social network analysis, whilst maintaining
+ anonymity of users.
+ 
+ ### Getting Started
+1. Place the Facebook.py script into your working directory.
+1. Check that the location of your geckodriver is in `/usr/local/bin/geckodriver` on
+Mac, or wherever your PATH is set to on Windows 
+(see [this guide](https://www.architectryan.com/2018/03/17/add-to-the-path-on-windows-10/)
+ to locate PATH on a Windows machine).
+For a guide to downloading geckodriver for Firefox, click 
+[here](http://lmgtfy.com/?q=geckodriver+download+for+firefox).
+1. Follow the code example below:
+```python
+from Facebook import *
+import unicodecsv as csv
+
+# Enter login info of an account.
+email = "phonyemail@aol.net"  
+password = "P@$$W0RD" 
+# At 5 events per page, the parameter 1 will return 5 events.
+number_of_pages = 1       
+
+# Create a Facebook object with your credentials, and a maximum wait time in seconds for each operation.
+fb = Facebook(email, password, max_wait=10, geckopath='/usr/local/bin/geckodriver')
+
+# Navigate to the mobile version of the site. Navigate with www.mobile.facebook.com on Firefox.
+fb.goto_page("https://mobile.facebook.com/SOME_PAGE/super_long_link...")
+
+df = fb.scrape_mobile(number_of_pages, scrape_who_reacted=True, hush_params=["21:30", "07:30", 30, 30])
+# Returns a nested list (ready to be iterated through and written to csv) of the format:
+# [text, num_likes, num_loves, num_hahas, num_wows, num_sads, num_angrys, num_reacts, event_link, hashtags_used(list), reacted(hashed list)]
+```
+
+### Disclaimer
+
+**I take it as a personal challenge to try to find loopholes in privacy and API's so that companies
+(_\*cough\* Facebook \*cough\*_) might learn to be a little more responsible with their data practices.**
+
+I am not responsible for any trouble you get yourself into because of this script. This is purely to
+educate the community about ways of getting around sticky API access to scrape data, and to demonstrate
+the power of Python! To learn more about the GDRP regulations, click
+[here](https://gdpr-info.eu/art-23-gdpr/).
